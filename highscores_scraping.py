46de30d3e6351488162a08883ced02fe4a2e274c
Ryan Hood <ryanchristopherhood@gmail.com>
2020-01-07 09:47:10 -0600

    Removed Print Statements

--- highscores_scraping.py
+++ highscores_scraping.py
@@ -19,9 +19,7 @@ def fetch_highscores(first_name):
     # To access a player's highscores, we must have RoboBrowser enter their username in the
     # 'search player' form.  There are multiple forms on the page, but the 'search player'
     # form is the first one.
-    print("HI")
     form = br.get_forms()[0]
-    print("HI")
 
     # 'user1' is the name attribute of the form.  We get the username from the first name using
     # the file 'usernames'.  The purpose of a separate file for usernames is so I can present
@@ -36,12 +34,11 @@ def fetch_highscores(first_name):
 
     # Here is use BeautifulSoup.  It's probably not necessary to switch from RoboBrowser, but
     # it was easier for me to get the code working.
-    soup = BeautifulSoup(src, 'html.parser')
+    soup = BeautifulSoup(src, features='html.parser')
 
     # At the time of writing this code, 'class_ = centerDiv' is the closest location for the
     # total level.
     posts = soup.find_all(class_ = 'centerDiv')
-    print(posts[0])
     player_string = posts[0].get_text()
 
     # 'player_string' has a lot of new line characters, and I only want one piece of the string.
